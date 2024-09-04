b9b883997e824edde9c45aa3b6a36a031aa93d91
Rahul <140081698+Rahuldec225@users.noreply.github.com>
2024-09-04 13:42:11 +0000

    datatype

---
+++ 01_basics/02_dataTypes.js
@@ -0,0 +1,37 @@
+"use strict";  // treat all js code as newer version
+
+// alert("hello")  // we are using node js not browser
+
+
+let name = "rahul"  //string datatype 
+let age = 3         //number datatype
+let isloggedin = true       // boolean datatype
+
+
+
+
+/*types of datatype
+
+premitive datatypes {
+
+number => 2 to power 53
+bigint
+string => ""
+boolean => true/false 
+null =>  standalone value
+undefined => value not assigned
+symbol => unique
+
+}
+
+
+non premitive datatype{
+
+
+}
+*/
+
+console.log(typeof undefined);  //undefined
+
+
+console.log(typeof null);  //object
