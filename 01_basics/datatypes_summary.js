4f668513acea40c56c1db1960a0a8d0a6abbe506
Rahul <140081698+Rahuldec225@users.noreply.github.com>
2024-09-05 20:17:55 +0000

    commit

---
+++ 01_basics/datatypes_summary.js
@@ -0,0 +1,50 @@
+// categories of datatype is based on how data is stored in memory and how it is accessed 
+/*
+        #premitive Datatype  (refernce)
+        {
+        String
+        Number
+        Boolean
+        Null
+        Undefined
+        Bigint 
+        Symbol
+        }
+
+
+        Non_Premitive Datatype (Reference)
+        {
+        Array
+        objects
+        Functions
+        }
+*/
+
+const id = Symbol('123')
+const anotherId = Symbol('123')
+ console.log(id == anotherId)
+
+
+const bigNumber = 564163516452n
+
+
+
+
+// Arrays
+
+const heros = ["asdg","hf", 232]
+
+
+// Objects
+
+let myObj = {
+    Name : "fduy",
+    age : 22
+}
+
+const myFunction = function(){
+    console.log("this is function")
+}
+
+
+console.log(typeof bigNumber )
