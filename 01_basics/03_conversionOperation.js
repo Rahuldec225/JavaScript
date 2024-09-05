3b94e0ec3d30257d12f98442742bbefe686766bf
Rahul <140081698+Rahuldec225@users.noreply.github.com>
2024-09-05 19:39:11 +0000

    conversion done

---
+++ 01_basics/03_conversionOperation.js
@@ -0,0 +1,43 @@
+let score = true
+
+
+// console.log(typeof score );
+// console.log(typeof (score));
+
+
+let valueInNumber = Number(score)
+
+// console.log(typeof valueInNumber)
+// console.log(valueInNumber)
+
+
+
+// "33"=> 33
+// "33sd"=> NaN
+// "true "=> 1  ,  "false" => 0
+
+
+let isLoggedIn = "dhfs"
+
+
+let booleanLoggedIn = Boolean(isLoggedIn)
+// console.log(booleanLoggedIn)
+
+
+
+let someNumber = 33
+let stringNumber = String(someNumber) 
+// console.log(stringNumber)
+
+// console.log(typeof stringNumber)
+
+
+
+
+//**********************************************Operations******************************************
+
+
+
+let value = 3
+let negValue = -value
+// console.log(negValue)
