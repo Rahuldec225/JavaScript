ffd619be3f9c9904dd3e2f64686574a161d41fb1
Rahul <140081698+Rahuldec225@users.noreply.github.com>
2024-09-10 19:03:45 +0000

    iteration done

---
+++ 05_iteration/two.js
@@ -0,0 +1,20 @@
+// let index = 0
+// while (index <=10) {
+//     console.log(`value of index is ${index}`)
+//     index = index + 2
+// }
+
+// let arr = 0
+// let myarr = ['flash', "batman", "superman"]
+// while (arr < myarr.length) {
+//     console.log(`value is ${myarr[arr]}`)
+//     arr = arr +1;
+// }
+
+
+let score =  1
+
+do {
+    console.log(`score : ${score}`);
+    score++
+} while (score <= 10);
