2e8f01acd202f8aafde04be87af8dc0e2413075c
Rahul <140081698+Rahuldec225@users.noreply.github.com>
2024-09-11 10:59:39 +0000

    dom

---
+++ 05_iteration/six.js
@@ -0,0 +1,26 @@
+const coding = ['js', "ruby", "java", "python", "cpp"]
+
+// const value = coding.forEach((item) => {
+//     console.log(item)
+//     return item
+// })
+
+// console.log(value)
+
+
+ const myNumbers = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
+
+// //const newnums = myNumbers.filter((num) => num > 4)
+// const newnums = myNumbers.filter((num) =>{ 
+//         return num > 4
+// })
+
+
+// const newNumbs = []
+// myNumbers.forEach((item) => {
+//     if (item > 4){
+//         newNumbs.push(item)
+//     }
+// })
+// console.log(newNumbs);
+
